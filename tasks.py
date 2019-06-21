1a1b340e3b680ace5e579ff94c14a05b39e86d7d
yzbx <youdaoyzbx@163.com>
2019-06-21 17:40:28 +0800

    update interface, add demo

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from utils import detection
+from flask import Flask,Response
+from utils import detection,video_player
 from flask import request, jsonify
 from flask import render_template,redirect,url_for
 import subprocess
@@ -14,22 +14,32 @@ app_config=[]
 
 @flask_app.route('/')
 def index():
-    return render_template('result.html',
-                            title='test',
+    date=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    return render_template('index.html',
+                            title='index',
                             pid=0,
                             status='?',
                             is_running='?',
-                            video_url='rtsp/xxx',
-                            task_name='detection',
-                            others='2019/05/28')
+                            video_url='rtsp://admin:juancheng1@221.1.215.254:554',
+                            task_name='detection_car',
+                            others='{"date":"%s"}'%date)
 
-def generate_error(code,str='',succeed=0,pid=None):
+@flask_app.route('/demo')
+def demo():
+    date=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    return render_template('demo.html',
+                            title='demo',
+                            video_url='rtsp://admin:juancheng1@221.1.215.254:554',
+                            task_name='detection_car',
+                            others='{"date":"%s"}'%date)
+
+def generate_error(code,app_name,video_url,error_string='',succeed=0,pid=None):
     if pid is None:
-        return {'succeed':succeed,'error_code':code,
-        'error_string':str}
+        return {'succeed':succeed,'app_name':app_name,'error_code':code,
+        'video_url':video_url,'error_string':error_string}
     else:
-        return {'succeed':succeed,'error_code':code,
-        'error_string':str,'pid':pid}
+        return {'succeed':succeed,'app_name':app_name,'error_code':code,
+        'video_url':video_url,'error_string':error_string,'pid':pid}
 
 def get_data(request,name):
     if request.method == 'POST':
@@ -37,7 +47,7 @@ def get_data(request,name):
     elif request.method == 'GET':
         value=request.args.get(name)
     else:
-        return False,0
+        return False,None
 
     return True,value
 
@@ -49,13 +59,16 @@ def get_app_id(data):
     
     return -1
 
-@flask_app.route('/longtask', methods=['POST', 'GET'])
-def longtask():
+@flask_app.route('/start_task', methods=['POST', 'GET'])
+def start_task():
     data={'video_url':None,'task_name':None,'others':None}
     for key in data.keys():
         flag,value=get_data(request,key)
         if not flag:
-            return json.dumps(generate_error(1,'cannot obtain data {}'.format(key)))
+            return json.dumps(generate_error(1,
+                                             app_name='start_task',
+                                             video_url=data['video_url'],
+                                             error_string='cannot obtain data {}'.format(key)))
         else:
             data[key]=value
 
@@ -64,14 +77,20 @@ def longtask():
     data['pid']=proc.pid
     assert data['pid']>0
     app_config.append(data)
-    return json.dumps(generate_error(0,succeed=1,pid=proc.pid))
+    return json.dumps(generate_error(0,succeed=1,pid=proc.pid,
+                                     app_name='start_task',
+                                     video_url=data['video_url']))
 
-@flask_app.route('/taskresult/<pid>')
-def taskresult(pid):
+@flask_app.route('/task_result/<pid>')
+def task_result(pid):
     try:
         p = psutil.Process(int(pid))
     except Exception as e:
-        return json.dumps(generate_error(3,succeed=1,pid=pid,str=e.__str__()))
+        return json.dumps(generate_error(3,
+                                         video_url='',
+                                         app_name='task_result',
+                                         succeed=1,pid=pid,
+                                         error_string=e.__str__()))
     
     return render_template('result.html',
                             title=pid,
@@ -82,8 +101,42 @@ def taskresult(pid):
                             task_name='detection',
                             others='2019/05/28')
 
-@flask_app.route('/stoptask',methods=['POST','GET'])
-def stoptask():
+@flask_app.route('/stop_task',methods=['POST','GET'])
+def stop_task():
+    data={'video_url':None,'task_name':None,'others':None}
+    for key in data.keys():
+        flag,value=get_data(request,key)
+        if not flag:
+            return json.dumps(generate_error(1,
+                                             video_url=data['video_url'],
+                                             app_name='stop_task',
+                                             error_string='cannot obtain data {}'.format(key)))
+        else:
+            data[key]=value
+
+    pid=get_app_id(data) 
+    if pid==-1:
+        return json.dumps(generate_error(2,video_url=data['video_url'],
+                                         app_name='stop_task',
+                                         error_string='no process running for {}/{}'.format(data['video_url'],data['task_name'])))
+
+    try:
+        p = psutil.Process(pid)
+        p.terminal()
+    except Exception as e:
+        return json.dumps(generate_error(3,
+                                         video_url=data['video_url'],
+                                         app_name='stop_task',
+                                         succeed=1,pid=pid,error_string=e.__str__()))
+    return  json.dumps(generate_error(0,
+                                      video_url=data['video_url'],
+                                      app_name='stop_task',
+                                      succeed=1,
+                                      pid=pid))
+        
+
+@flask_app.route('/start_demo')
+def start_demo():
     data={'video_url':None,'task_name':None,'others':None}
     for key in data.keys():
         flag,value=get_data(request,key)
@@ -91,19 +144,10 @@ def stoptask():
             return json.dumps(generate_error(1,'cannot obtain data {}'.format(key)))
         else:
             data[key]=value
-
-    pid=get_app_id(data) 
-    if pid==-1:
-        return json.dumps(generate_error(2,'no process running for {}/{}'.format(data['video_url'],data['task_name'])))
-
-    try:
-        p = psutil.Process(pid)
-        p.terminal()
-    except Exception as e:
-        return json.dumps(generate_error(3,succeed=1,pid=pid,str=e.__str__()))
-    return  json.dumps(generate_error(0,succeed=1,pid=pid))
-        
     
+    return Response(video_player(data['video_url']).gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+        
 if __name__ == '__main__':
     parser=argparse.ArgumentParser()
     parser.add_argument('-p','--port',help='port for web application',default=5005,type=int)
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import time
 import json
+import cv2
 def detection(data_json):
     data=json.loads(data_json)
     for i in range(100):
@@ -7,3 +8,23 @@ def detection(data_json):
         time.sleep(1)
 
     return 0
+
+class video_player():
+    def __init__(self,video_url):
+        self.video=cv2.VideoCapture(video_url)
+        
+    def __del__(self):
+        self.video.release()
+        
+    def get_frame(self):
+        success, image = self.video.read()
+        ret, img = cv2.imencode('.jpg', image)
+        return img.tobytes()
+    
+    def gen(self):
+        idx=0
+        while idx<1000:
+            frame=self.get_frame()
+            idx+=1
+            yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
