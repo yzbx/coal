8471a32deabdd9ab0b7ed28f87ec18710f6d991f
yzbx <youdaoyzbx@163.com>
2019-06-26 17:23:58 +0800

    fix bug

--- web_utils.py
+++ web_utils.py
@@ -14,10 +14,11 @@ class app_player():
     def __init__(self,video_url,app='yolov3',show_full_img=False):
         self.video_url=video_url
         self.app=app
+        self.show_full_img=show_full_img
         
     def gen(self):
         if self.app=='yolov3':
-            for img in yolov3_detect(self.video_url,show_full_img):
+            for img in yolov3_detect(self.video_url,self.show_full_img):
                 ret, img = cv2.imencode('.jpg', img)
                 frame=img.tobytes()
                 yield (b'--frame\r\n'
